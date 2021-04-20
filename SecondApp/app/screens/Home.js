b51da026b8bc361291fe9f3ef317341034150ebb
Chief-boy-117 <67178769+Chief-boy-117@users.noreply.github.com>
2021-04-20 17:53:27 +0530

    Create Home.js

---
+++ SecondApp/app/screens/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, Text, StyleSheet, Button} from "react-native";
+
+const Home = ({navigation}) => {
+    return(
+
+        <View style={styles.container}>
+            <Button 
+            title="Settings" 
+            onPress={()=>navigation.navigate('Settings')}/>
+        </View>
+    )}
+    export default Home;
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center'
+        }
+    });
